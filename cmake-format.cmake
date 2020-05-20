85fbf6ed6585eface1dc112d8abef307a7678e94
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2020-05-19 21:55:36 -0400

    implement the other targets

--- cmake-format.cmake
+++ cmake-format.cmake
@@ -27,8 +27,20 @@ if (CMAKE_FORMAT_TARGET STREQUAL fix-cmake-format)
   return()
 endif()
 
-if (CMAKE_FORMAT_TARGET STREQUAL cmake-format)
 
-elseif (CMAKE_FORMAT_TARGET STREQUAL check-cmake-format)
-
+if (CMAKE_FORMAT_TARGET STREQUAL check-cmake-format)
+  set(OUTPUT_QUIET OUTPUT_QUIET)
 endif()
+
+set(formatted_cmake_file ${BINARY_DIR}/formatted.cmake)
+foreach(cmake_file IN LISTS CMAKE_FILES)
+  set(source_cmake_file ${CMAKE_SOURCE_DIR}/${cmake_file})
+  execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${source_cmake_file} ${formatted_cmake_file})
+  execute_process(COMMAND ${CMAKE_FORMAT_PROGRAM} -c ${CONFIG_FILE} -i ${formatted_cmake_file})
+  execute_process(COMMAND ${GIT_EXECUTABLE} diff --color --no-index -- ${source_cmake_file} ${formatted_cmake_file}
+    RESULT_VARIABLE result
+    ${OUTPUT_QUIET})
+  if (OUTPUT_QUIET AND result)
+    message(FATAL_ERROR "${cmake_file} needs to be reformatted")
+  endif()
+endforeach()
