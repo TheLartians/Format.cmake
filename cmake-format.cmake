0466c437058361dc002cfa090dfa138a1c7efaf9
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2020-05-20 15:01:36 -0400

    remove config files logic

--- cmake-format.cmake
+++ cmake-format.cmake
@@ -10,24 +10,8 @@ endfunction()
 
 filter_files(GIT_REPOSITORY_DIR ${CMAKE_SOURCE_DIR} OUTPUT_LIST CMAKE_FILES REGEX "\\.cmake$|(^|/)CMakeLists\\.txt$")
 
-# Find cmake-format configs
-separate_arguments(CMAKE_MODULE_PATH)
-foreach (dir IN LISTS CMAKE_MODULE_PATH CMAKE_SOURCE_DIR)
-  filter_files(GIT_REPOSITORY_DIR ${dir} OUTPUT_LIST modules_configs REGEX "(^|/)\\.?cmake-format\\.(yaml|json|py)$")
-  foreach (cfg IN LISTS modules_configs)
-    list(APPEND CONFIG_FILES_ARG ${dir}/${modules_configs})
-  endforeach()
-endforeach()
-
-if (CONFIG_FILES_ARG)
-  list(PREPEND CONFIG_FILES_ARG --config-files)
-endif()
-
-set(CONFIG_FILE ${BINARY_DIR}/cmake-format.py)
-execute_process(COMMAND ${CMAKE_FORMAT_PROGRAM} ${CONFIG_FILES_ARGS} --dump-config OUTPUT_FILE ${CONFIG_FILE})
-
 if (CMAKE_FORMAT_TARGET STREQUAL fix-cmake-format)
-  execute_process(COMMAND ${CMAKE_FORMAT_PROGRAM} -c ${CONFIG_FILE} -i ${CMAKE_FILES})
+  execute_process(COMMAND ${CMAKE_FORMAT_PROGRAM} -i ${CMAKE_FILES})
   return()
 endif()
 
@@ -39,7 +23,7 @@ set(formatted_cmake_file ${BINARY_DIR}/formatted.cmake)
 foreach(cmake_file IN LISTS CMAKE_FILES)
   set(source_cmake_file ${CMAKE_SOURCE_DIR}/${cmake_file})
   execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${source_cmake_file} ${formatted_cmake_file})
-  execute_process(COMMAND ${CMAKE_FORMAT_PROGRAM} -c ${CONFIG_FILE} -i ${formatted_cmake_file})
+  execute_process(COMMAND ${CMAKE_FORMAT_PROGRAM} -i ${formatted_cmake_file})
   execute_process(COMMAND ${GIT_EXECUTABLE} diff --color --no-index -- ${source_cmake_file} ${formatted_cmake_file}
     RESULT_VARIABLE result
     ${OUTPUT_QUIET})
