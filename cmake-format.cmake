0955ae465e18f7c618319bac91d01daeda92afc4
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2020-05-24 13:43:46 -0400

    format with the corrent config file

--- cmake-format.cmake
+++ cmake-format.cmake
@@ -25,8 +25,7 @@ endif()
 set(formatted_cmake_file ${BINARY_DIR}/formatted.cmake)
 foreach(cmake_file IN LISTS CMAKE_FILES)
   set(source_cmake_file ${CMAKE_SOURCE_DIR}/${cmake_file})
-  execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${source_cmake_file} ${formatted_cmake_file})
-  execute_process(COMMAND ${CMAKE_FORMAT_PROGRAM} -i ${formatted_cmake_file})
+  execute_process(COMMAND ${CMAKE_FORMAT_PROGRAM} -i ${source_cmake_file} -o ${formatted_cmake_file})
   execute_process(COMMAND ${GIT_PROGRAM} diff --no-index -- ${source_cmake_file} ${formatted_cmake_file}
     RESULT_VARIABLE result
     ${OUTPUT_QUIET})
