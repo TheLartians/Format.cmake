5dc6b4953b9ed697632654777e5ffe4298ce7aa8
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2020-05-25 13:50:23 -0400

    format pairs per line

--- cmake-format.cmake
+++ cmake-format.cmake
@@ -1,14 +1,19 @@
 # Regex-filter a git repository's files.
 function (filter_files)
   cmake_parse_arguments("" "" "GIT_REPOSITORY_DIR;OUTPUT_LIST;REGEX" "" ${ARGN})
-  execute_process(COMMAND ${GIT_PROGRAM} ls-files --cached --exclude-standard WORKING_DIRECTORY ${_GIT_REPOSITORY_DIR} OUTPUT_VARIABLE all_files)
+  execute_process(COMMAND ${GIT_PROGRAM} ls-files --cached --exclude-standard
+    WORKING_DIRECTORY ${_GIT_REPOSITORY_DIR}
+    OUTPUT_VARIABLE all_files)
   cmake_policy(SET CMP0007 NEW)
   string(REPLACE "\n" ";" filtered_files "${all_files}")
-  list(FILTER filtered_files INCLUDE REGEX ${_REGEX})
+  list(FILTER filtered_files
+    INCLUDE REGEX ${_REGEX})
   set(${_OUTPUT_LIST} ${filtered_files} PARENT_SCOPE)
 endfunction()
 
-filter_files(GIT_REPOSITORY_DIR ${CMAKE_SOURCE_DIR} OUTPUT_LIST CMAKE_FILES REGEX "\\.cmake$|(^|/)CMakeLists\\.txt$")
+filter_files(GIT_REPOSITORY_DIR ${CMAKE_SOURCE_DIR}
+    OUTPUT_LIST CMAKE_FILES
+    REGEX "\\.cmake$|(^|/)CMakeLists\\.txt$")
 
 if (CMAKE_FORMAT_TARGET STREQUAL fix-cmake-format)
   execute_process(COMMAND ${CMAKE_FORMAT_PROGRAM} -i ${CMAKE_FILES})
