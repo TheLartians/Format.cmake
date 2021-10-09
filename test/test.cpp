fc9cdeeaf2007f66e88353bcc6e3084cdb8a195e
Lars Melchior <TheLartians@users.noreply.github.com>
2021-10-09 18:15:39 +0200

    unformat test file

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
-int main() {
-  std::cout << "Hello World!" << std::endl;
-  return 0;
-}
+                    #include <iostream>
+                  int
+              main
+            (
+          )
+        {
+      std::
+    cout
+  <<
+"Hello World!"
+  <<
+    std::
+      endl
+        ;
+          return
+            0
+              ;
+                }
