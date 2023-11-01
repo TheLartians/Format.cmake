db37c4eaf7dea7c4a66f8b03aec461725c474792
Gary MARIGLIANO <5860372+krypty@users.noreply.github.com>
2023-11-01 17:27:14 +0100

    Add option to specify extra arguments for cmake-format (#37)
    The arguments are documented here: https://github.com/cheshirekow/cmake_format#usage

--- cmake-format.cmake
+++ cmake-format.cmake
@@ -37,8 +37,10 @@ get_cmake_files(
   "\\.cmake$|(^|/)CMakeLists\\.txt$"
 )
 
+separate_arguments(CMAKE_FORMAT_EXTRA_ARGS)
+
 if(CMAKE_FORMAT_TARGET STREQUAL fix-cmake-format)
-  execute_process(COMMAND ${CMAKE_FORMAT_PROGRAM} -i ${CMAKE_FILES})
+  execute_process(COMMAND ${CMAKE_FORMAT_PROGRAM} ${CMAKE_FORMAT_EXTRA_ARGS} -i ${CMAKE_FILES})
   return()
 endif()
 
@@ -49,7 +51,11 @@ endif()
 set(formatted_cmake_file ${BINARY_DIR}/formatted.cmake)
 foreach(cmake_file IN LISTS CMAKE_FILES)
   set(source_cmake_file ${CMAKE_SOURCE_DIR}/${cmake_file})
-  execute_process(COMMAND ${CMAKE_FORMAT_PROGRAM} -o ${formatted_cmake_file} ${source_cmake_file})
+  execute_process(
+    COMMAND ${CMAKE_FORMAT_PROGRAM} ${CMAKE_FORMAT_EXTRA_ARGS} -o ${formatted_cmake_file}
+            ${source_cmake_file}
+  )
+
   execute_process(
     COMMAND ${GIT_PROGRAM} diff -G. --color --no-index -- ${source_cmake_file}
             ${formatted_cmake_file} RESULT_VARIABLE result ${OUTPUT_QUIET_OPTION}
